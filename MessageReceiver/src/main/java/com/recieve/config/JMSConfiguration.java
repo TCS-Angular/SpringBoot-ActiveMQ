191e45cc299e6041f7fd5b6fee1de653e49d290d
ngactive99 <ngactive99@gmail.com>
2022-06-16 16:26:17 +0530

    message reciver project created and model and config added

---
+++ MessageReceiver/src/main/java/com/recieve/config/JMSConfiguration.java
@@ -0,0 +1,40 @@
+package com.recieve.config;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "com.recieve")
+public class JMSConfiguration {
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// this provides all the spring boot's default config to this factory, including
+		// the message converter
+		configurer.configure(factory, connectionFactory);
+		// you can still override some of boot's default if nessaARY
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+}
