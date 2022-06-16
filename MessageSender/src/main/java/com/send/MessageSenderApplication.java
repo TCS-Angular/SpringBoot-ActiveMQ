1cb2773db73ee9e35097761bd796f4d86d4296c9
ngactive99 <ngactive99@gmail.com>
2022-06-16 15:48:52 +0530

    message sender done

--- MessageSender/src/main/java/com/send/MessageSenderApplication.java
+++ MessageSender/src/main/java/com/send/MessageSenderApplication.java
@@ -2,12 +2,38 @@ package com.send;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.send.model.Product;
 
 @SpringBootApplication
+@EnableJms
 public class MessageSenderApplication {
+	
+	private static final String MESSAGE_QUEUE = "message_queue";
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessageSenderApplication.class, args);
+		// launch the application
+		ConfigurableApplicationContext context = SpringApplication.run(MessageSenderApplication.class, args);
+		
+		// to get the jms template bean reference 
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		
+		for(int i = 1; i<= 10; i++) {
+			Product product = new Product();
+			product.setProductId(i);
+			product.setName("Washing Machine");
+			product.setQuantity(i);
+			
+			// send a message 
+			System.out.println("Sending a product " + i);
+			jmsTemplate.convertAndSend(MESSAGE_QUEUE, product);
+			
+		}
+		
+		
 	}
 
 }
---
+++ MessageSender/src/main/java/com/send/config/JMSConfiguration.java
@@ -0,0 +1,37 @@
+package com.send.config;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JMSConfiguration {
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		// this provides all the spring boot's default config to this factory, including
+		// the message converter
+		configurer.configure(factory, connectionFactory);
+		// you can still override some of boot's default if nessaARY
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+}
